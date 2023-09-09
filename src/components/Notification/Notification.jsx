aad86f0ec36324f057961eff81b9e60faa13e345
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-10 00:48:38 +0300

    Update Notification.jsx

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,7 +2,10 @@ import PropTypes from 'prop-types';
 import Notiflix from 'notiflix';
 
 export function Notification({ message }) {
-  return Notiflix.Notify.failure(message);
+  return Notiflix.Notify.info(message, {
+    position: 'center-center',
+    fontSize: '16px',        
+  });
 }
 
 Notification.propTypes = {
